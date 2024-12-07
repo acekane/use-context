d96518f000806e92be62cfad5c2598a7020cd918
Deep <145989362+acekane@users.noreply.github.com>
2024-12-07 14:26:25 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Card from './Compenent/Card'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+       <div>
+       <Card/>
+       </div>
+    </>
+  )
+}
+
+export default App
